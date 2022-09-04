8cf1f2a77be6ec18bf2586d38f6931c294c3dcc1
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-09-04 14:28:40 -0300

    exercicio 01

---
+++ nelio-alves-java/02/src/ExercicioUm.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ExercicioUm {
+	public static void main(String[] args) {
+		int x, y, soma;
+
+		Scanner sc = new Scanner(System.in);
+
+		x = sc.nextInt();
+		y = sc.nextInt();
+
+		soma = x + y;
+
+		System.out.println("SOMA = " + soma);
+	}
+}
